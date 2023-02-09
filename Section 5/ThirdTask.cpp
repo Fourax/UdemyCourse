b043ebe6cbca97a530709077d3f4359b53820b5c
Fourax <bartoszkialka4@gmail.com>
2023-02-09 22:10:26 +0100

    Section 5 with exam tasks

---
+++ Section 5/ThirdTask.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+void date_of_birth() {
+    int m {};
+    int d {};
+    int y {};
+    
+    cin >> m;
+    cin >> d;
+    cin >> y;
+    
+    cout << m << " " << d << " " << y;
+    
+}
