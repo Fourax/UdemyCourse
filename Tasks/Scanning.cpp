e6d81862c86ef15f04eebc990ddf4849ab5b8c9c
Fourax <bartoszkialka4@gmail.com>
2023-02-20 22:55:33 +0100

    Bubble sorting in progress

--- Tasks/Scanning.cpp
+++ Tasks/Scanning.cpp
@@ -1,27 +1,28 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include <cmath>
-using namespace std; 
- 
-const int sz = 10; // set sz to max size of the array 
-int arr[sz]; 
- 
-int main() 
-{ 
-    freopen("numbers.txt","r", stdin);  
-    int index = 0, num; 
-     
-    /// scan till end of File 
-     
-    while(scanf("%d",&num) == 1) { 
-        arr[index++] = num; 
-    } 
- 
-    for(int i = 0; i<index; i++) 
-        printf("%d ",arr[i]); 
-    printf("\n"); 
-     
-     
-    return 0; 
-} 
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+int main () {
+  string line;
+  ifstream myFile ("numbers.txt");
+  ofstream newFile("sorted.txt");
+  int myArray [10];
+  if (myFile.is_open())
+  {
+    int index = 0;
+    while (! myFile.eof() )
+    {
+      getline (myFile,line);
+    myArray[index++] << line;
+    //  cout << line << endl;
+    
+    }
+    myFile.close();
+  }
+
+  else cout << "Unable to open file"; 
+
+  return 0;
+}
