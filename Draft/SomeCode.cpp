ca5b74c033eb40c64cd8db0285b5a1505f3eebb0
Fourax <bartoszkialka4@gmail.com>
2023-03-03 23:55:52 +0100

    Switch Satements and task from section9

--- Draft/SomeCode.cpp
+++ Draft/SomeCode.cpp
@@ -310,5 +310,101 @@ cout << "\n -------------------Shopping example---------------------------------
 
 
 cout << "\n==================================Nested If Satements========================================================"<<endl;
+
+    char letter_grade{}; // trzeba uzyc srednika zapytac czemu 
+
+    cout <<"Enter the letter grade you expect on the exam: ";
+    cin >> letter_grade;
+
+    switch(letterGrade)
+    {
+        case 'a':
+        case 'A':
+        
+        cout << "You need a 90 or above, study hard !";
+        
+        break;
+
+        case 'b':
+        case 'B':
+        
+        cout << "You need 80 -89 for a B, go study!"<<endl;
+        
+        break;
+        
+        case 'c':
+        case 'C':
+
+        cout << "You need 70-79 for an average grade";
+        
+        break;
+
+        case 'd':
+        case 'D':
+        cout << " Hmm,you should strive for a better grade.All you need is 60-69"<<endl;
+
+        break;
+        
+        case 'f':
+        case 'F':
+       {
+        char confirm{};
+        cout << " Are you sure (Y/N)";
+        cin >> confirm;
+        if(confirm == 'y' || confirm == 'Y')
+        {
+             cout << "Ok, I guess you didnt study..."<<endl;
+        }
+        else
+        {
+            if (confirm == 'n' || confirm == 'N')
+            {
+                cout << "Good - go study!"<<endl;
+            }
+            else
+            {
+                cout <<"Illegal chice"<<endl;
+            }
+
+        }
+            break;
+       }
+
+        
+
+        default:
+        cout << "Sorry, not a valid grade"<< endl;
+    }
+
+    cout << "\n Switch with enumeration"<<endl;
+
+    enum Direction
+    {
+        left,right,up,down
+    };
+    
+    Direction heading {left};
+
+    switch(heading)
+    {
+        case left:
+        
+        cout << "Going left"<<endl;
+        
+        break;
+
+        case right:
+
+        cout << "Going  right"<<endl;
+
+        break;
+
+        default:
+        cout <<"OK"<< endl;
+
+    }
+
+
+cout << "\n==================================Switch Satements========================================================"<<endl;
   return 0;
 }
