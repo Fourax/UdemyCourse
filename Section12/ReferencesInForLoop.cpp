cc08279c08fddd04c56c5f1904f7d9e42de078c3
Fourax <bartoszkialka4@gmail.com>
2023-05-07 20:17:08 +0200

    Using references in for loop

---
+++ Section12/ReferencesInForLoop.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+    vector<string> stooges {"Larry","Moe","Curly"};\
+
+    for(auto str:stooges)
+    {
+        str = "Funny"; //str skopuje kazdy element wektora
+    }
+    
+    for(auto str:stooges)
+    {
+        cout << str << endl;
+    }
+    
+    cout <<"\n-----------------------------"<<endl;
+
+    for(auto &str:stooges) // str jest referencjom do kazdego elementu wektora
+    {
+        str = "Funny";
+    }
+    
+    for(auto const &str:stooges)//
+    {
+        cout << str << endl;
+    }
+    return 0;
+}
