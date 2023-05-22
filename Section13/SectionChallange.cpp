7cd3bd22db6b747b12736623940ca0e3a45d608d
Fourax <bartoszkialka4@gmail.com>
2023-05-22 17:51:00 +0200

    Pobably done section challange but i cant compile becasue showsup new vs code problem

--- Section13/SectionChallange.cpp
+++ Section13/SectionChallange.cpp
@@ -3,13 +3,33 @@
 #include <string>
 using namespace std;
 
-class Movies
+class Movie
 {
 private:
-    std::vector<Movie> movies;
+    vector<Movie> movies;
+    string name;
+    string rating;
+    int watched;
 public:
-    Movies();
-    ~Movies();
+    Movie ();
+    
+    //Constructor
+    Movie(string name, string rating, int watched);
+    //Copy constructor
+    Movie(const Movie &source);
+    //Deconstructor
+    ~Movie();
+    
+    void setName(string name){this -> name = name;}
+    string getName()const{return name;}
+
+    void setRating(string rating){this -> rating = rating;}
+    string getRating()const{return rating;}
+
+    void setWatched(int watched){this -> watched = watched;}
+    int getWatched() const{return watched;}
+
+    void incrementWatched(){++watched;}
 
     bool addMovie(std:: string name, std::string rating, int watched);
 
@@ -17,23 +37,86 @@ public:
 
     void display() const;
 };
-Movie::Movies()
+
+Movie::Movie(string name, string rating, int watched): name(name), rating(rating), watched(watched)
 {
 
 }
-Movies::~Movies()
+
+Movie::Movie(const Movie &source):Movie{source.name, source.rating, source.watched}
 {
 
 }
 
-bool Movies::addMovie(std::string name, std::string rating, int watched)
+Movie::~Movie()
 {
-return flase;
+
 }
 
-bool Movies::incrementWatched(std::string name)
+void Movie::display() const
 {
-return false;
+    cout << name << "," << rating << watched << endl;
 }
 
+bool Movie::addMovie(std::string name, std::string rating, int watched)
+{
+    return false;
+}
 
+bool Movie::incrementWatched(std::string name)
+{
+    return false;
+} 
+
+void incrementWatched(Movie &movies, std::string name);
+
+void addMovie(Movie &movies, std:: string name, std:: string rating, int watched);
+
+void incrementWatched(Movie &movies, std::string name)
+{
+    if(movies.incrementWatched(name))
+    {
+        cout << name <<"Watch incremented"<< endl;
+    }
+    else
+    {
+        cout << name <<"not found" << endl;
+    }
+}
+void addMovie(Movie &movies, std:: string name, std:: string rating, int watched)
+{
+    if(movies.addMovie(name,rating,watched))
+    {
+        cout << name << "added" << endl;
+    }
+    else
+    {
+        cout << name <<"already exist" << endl;
+    }
+}
+ int main ()
+ {
+    Movie myMovies;
+
+    myMovies.display();
+
+    addMovie(myMovies,"Big","PG-13",2);
+    addMovie(myMovies,"Star Wars","PG",5);
+    addMovie(myMovies,"Cinderella","PG",7);
+
+    myMovies.display();
+
+    addMovie(myMovies,"Cinderella","PG",7);
+    addMovie(myMovies,"Ice Age","PG",12);
+    
+    myMovies.display();
+
+    incrementWatched(myMovies,"Big");
+    incrementWatched(myMovies,"Ice Age");
+
+    myMovies.display();
+
+    incrementWatched(myMovies,"XXXXXXXX");
+
+    return 0;
+ }
